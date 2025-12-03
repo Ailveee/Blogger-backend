f096c19144c5b7c4339fc38e5d8aa56647c5f8a8
Rayen Ben Ali <rayenbenali.pro@outlook.com>
2025-12-03 21:14:06 +0100

    Second phase of editing

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+            ])
+            // plainPassword is not mapped to the Entity (we hash it manually)
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(message: 'Please enter a password'),
+                    new Length(min: 6, max: 4096),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
